b5e9419bfb6f12d49b93a5c33a55edf939cfad67
akhil-sagar <nani5202220@gmail.com>
2025-07-06 15:40:12 +0530

    complete user update api

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 
 dotenv.config();
@@ -19,6 +20,7 @@ app.use(bodyParser.urlencoded())
 app.use(bodyParser.json())
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/user',userRouter);
 
