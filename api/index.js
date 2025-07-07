70992f48bbfc98ebfcd94e67f363563415cbe507
akhil-sagar <nani5202220@gmail.com>
2025-07-07 12:27:25 +0530

    create listing api route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import bodyParser from 'body-parser';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import listingRouter from './routes/listing.route.js';
 
 
 dotenv.config();
@@ -15,16 +16,20 @@ mongoose.connect(process.env.MONGO).then(()=>{
 })
 
 const app=express();
+
 app.use(bodyParser.urlencoded())
 
 app.use(bodyParser.json())
 
 app.use(express.json());
+
 app.use(cookieParser());
 
 app.use('/api/user',userRouter);
 
 app.use('/api/auth',authRouter);
+
+app.use('/api/listing',listingRouter)
 
 app.use((err ,req ,res ,next)=>{
     const statusCode=err.statusCode || 500;
